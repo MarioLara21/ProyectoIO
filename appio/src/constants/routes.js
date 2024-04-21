f56047352f5af35a4cd0697993a36e18b1629f12
Kev1606 <kevincruz1606@estudiantec.cr>
2024-04-21 11:21:09 -0600

    feat: rutas actualizadas para el tercer algoritmo

--- appio/src/constants/routes.js
+++ appio/src/constants/routes.js
@@ -1,13 +1,16 @@
 import Menu from '../Menu/Menu';
 import NodeInputWindow from '../FloydsAlgorithm/NodeInputWindow';
 import FloydsAlgorithm from "../FloydsAlgorithm/FloydsAlgorithm"
+import DataCollection from '../EquipmentReplacement/DataCollection';
+import ReplacementAlgorithm from '../EquipmentReplacement/ReplacementAlgorithm';
 
 export const routes = [
     { path: '/', element: < Menu /> },
     { path: '/floyds-input', element: < NodeInputWindow /> },
     { path: '/floyds-algorithm', element: < FloydsAlgorithm /> },
     // { path: '/knapsack-problem', component: KnapsackProblem },
-    // { path: '/equipment-replacement', component: EquipmentReplacement },
+    { path: '/equipment-input', element: < DataCollection /> },
+    { path: '/equipment-replacement', element: < ReplacementAlgorithm /> },
     // { path: '/optimal-binary-search-trees', component: OptimalBinarySearchTrees },
     // { path: '/sports-series', component: SportsSeries },
     // { path: '/matrix-multiplication', component: MatrixMultiplication },
